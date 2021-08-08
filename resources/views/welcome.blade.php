8a8463dcdd89fda15c594c434e2669a37978068d
ryosuke.oniki <ro10210326@gmail.com>
2021-08-08 23:23:37 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <div class="text-center">
             <h1>Welcome to the Selp</h1>
             <h2>~Let's share your goals and posts to improve yourself~</h2>
+             {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
